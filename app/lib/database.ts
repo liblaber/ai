58bb6e8b51b39e52abdc479be43d242ccbcc26c7
Stevan Kosijer <stkosijer@gmail.com>
2025-06-23 19:44:41 +0200

    Data accessor query execute should be awaited to have proper error handling. (#26)

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -3,15 +3,18 @@ import { logger } from '~/utils/logger';
 
 export async function executeQuery(connectionUrl: string, query: string, params?: string[]): Promise<any[]> {
   const dataAccessor = DataAccessor.getAccessor(connectionUrl);
-  await dataAccessor.initialize(connectionUrl);
-  dataAccessor.guardAgainstMaliciousQuery(query);
 
   try {
-    return dataAccessor.executeQuery(query, params);
+    await dataAccessor.initialize(connectionUrl);
+    dataAccessor.guardAgainstMaliciousQuery(query);
+
+    return await dataAccessor.executeQuery(query, params);
   } catch (e) {
     logger.error('Error executing query:', { error: e, query });
     throw e;
   } finally {
-    await dataAccessor.close();
+    if (dataAccessor) {
+      await dataAccessor.close();
+    }
   }
 }
