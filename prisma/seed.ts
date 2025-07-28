9d8448f6e94391f34c90fe58cfc1dd599d7ac8ab
Stevan Kapicic <kapicic.ste1@gmail.com>
2025-07-28 13:42:14 +0200

    Fix user seed logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,44 +10,47 @@ async function seed() {
   };
 
   try {
-    const existingOrganization = await prisma.organization.findUnique({
+    let organization = await prisma.organization.findUnique({
       where: { domain: anonymousOrganization.domain },
     });
 
-    if (existingOrganization) {
-      console.log('✅ Anonymous already exists');
-      return;
+    if (!organization) {
+      organization = await prisma.organization.create({
+        data: anonymousOrganization,
+      });
     }
 
-    const organization = await prisma.organization.create({
-      data: anonymousOrganization,
+    let anonUser = await prisma.user.findUnique({
+      where: {
+        email: 'anonymous@anonymous.com',
+      },
     });
 
-    // Create anonymous user
-    const anonymousUser = {
-      id: 'TYOjutAGl9gxV4b2jbBG2loaohynFnFs',
-      email: 'anonymous@anonymous.com',
-      name: 'Anonymous',
-      emailVerified: false,
-      organizationId: organization.id,
-      role: UserRole.ADMIN,
-      isAnonymous: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    if (!anonUser) {
+      // Create anonymous user
+      const anonymousUser = {
+        id: 'TYOjutAGl9gxV4b2jbBG2loaohynFnFs',
+        email: 'anonymous@anonymous.com',
+        name: 'Anonymous',
+        emailVerified: false,
+        organizationId: organization.id,
+        role: UserRole.ADMIN,
+        isAnonymous: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
 
-    const user = await prisma.user.create({
-      data: anonymousUser,
-    });
-
-    console.log('✅ Created anonymous user:', user.email);
+      anonUser = await prisma.user.create({
+        data: anonymousUser,
+      });
+    }
 
     await prisma.account.create({
       data: {
         id: 'kOK6L1dtMXjn9I09QtBFDYoX0aIT1ZL6',
-        accountId: user.id,
+        accountId: anonUser.id,
         providerId: 'credential',
-        userId: user.id,
+        userId: anonUser.id,
         password:
           '7545921c88db4437a061a113885545fd:6485b22048dc74d8548c1af7528a2b7cebaa921789f4ed95afc2616963172999fa5751c48829760b20d4bd48876ba9d2451c33ddb9bd3255b9842a1bbc89d0f3',
         createdAt: new Date(),
