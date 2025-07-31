c112a11b9ced314576603508c00eeadfd670ac27
Cameron Steele <cam.steeleis@gmail.com>
2025-07-31 09:50:10 -0500

    Refactor NEXT_PUBLIC_DISABLE_TELEMETRY to use JSON parsing for boolean conversion

--- app/env/client.ts
+++ app/env/client.ts
@@ -11,28 +11,10 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_KEY: z.string().optional(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().optional(),
     NEXT_PUBLIC_DISABLE_TELEMETRY: z
-      .string()
-      .transform((val, ctx) => {
-        const lowerCaseVal = val.toLowerCase();
-
-        if (lowerCaseVal === 'true' || lowerCaseVal === '1') {
-          return true;
-        }
-
-        if (lowerCaseVal === 'false' || lowerCaseVal === '0') {
-          return false;
-        }
-
-        // If it's not a recognized boolean string, add an issue
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message:
-            'Invalid boolean string for NEXT_PUBLIC_DISABLE_TELEMETRY. Expected "true", "True", "TRUE", "1", "false", "False", "FALSE", or "0".',
-        });
-
-        return z.NEVER; // Indicates an unresolvable type, signaling an error
-      })
-      .default('false'),
+      .preprocess((value) => {
+        return JSON.parse(value as string);
+      }, z.boolean())
+      .default(false),
     NEXT_PUBLIC_GITHUB_ACCESS_TOKEN: z.string().optional(),
   },
   runtimeEnv: {
