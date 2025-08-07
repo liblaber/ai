c12af9773296de0b11ae27d48a5a9ae7965816ca
Stevan Kosijer <stkosijer@gmail.com>
2025-08-07 19:18:20 +0200

    Always show docker logs after test run, for debugging

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -43,7 +43,7 @@ async function seed() {
         data: anonymousUser,
       });
 
-      console.log('😎Created anonymous user\n');
+      console.log('😎 Created anonymous user\n');
     }
 
     const account = await prisma.account.findFirst({
