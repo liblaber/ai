2fe9966979da2be2ced266bfdfa2bcc3fac629d9
Stevan Kosijer <stkosijer@gmail.com>
2025-08-08 16:14:23 +0200

    Redirect after successful login

--- app/components/DataLoader.tsx
+++ app/components/DataLoader.tsx
@@ -145,7 +145,7 @@ export function DataLoader({ children, rootData }: DataLoaderProps) {
       }
 
       console.log('✅ Anonymous login successful');
-      router.push('/');
+      window.location.replace('/');
     } catch (error: any) {
       console.error(`❌ Anonymous login failed: ${error?.message}`);
     } finally {
