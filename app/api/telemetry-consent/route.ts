24c0502a35b62225a60d62e8531e42f2f1144973
Stevan Kosijer <stkosijer@gmail.com>
2025-08-08 19:28:31 +0200

    Remove logs

--- app/api/telemetry-consent/route.ts
+++ app/api/telemetry-consent/route.ts
@@ -6,10 +6,6 @@ import { getTelemetry, TelemetryEventType } from '~/lib/telemetry/telemetry-mana
 
 export async function POST(request: NextRequest) {
   try {
-    // TODO: @skos just troubleshooting
-    const buba = await userService.getUserByEmail('anonymous@anonymous.com');
-    logger.info(`Anonymous user telemetry: ${buba.email}`, JSON.stringify(buba));
-
     const userId = await requireUserId(request);
 
     const body = await request.json();
