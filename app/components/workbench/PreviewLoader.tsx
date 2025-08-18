edd4c8196a09c14646d9086cc95c93277587cc29
Milan Lazić <90383105+mlanlazc@users.noreply.github.com>
2025-08-18 16:09:17 +0200

    Show error screen instead of the loader when code generation fails (#79)

--- app/components/workbench/PreviewLoader.tsx
+++ app/components/workbench/PreviewLoader.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { workbenchStore } from '~/lib/stores/workbench';
+import { GenerationError } from './GenerationError';
+
 type Props = {
   message?: string;
 };
@@ -7,29 +10,35 @@ type Props = {
 export const PreviewLoader = ({ message = 'Your app is loading...' }: Props) => {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <div className="bg-gradient-to-r from-gray-600 from-30% via-gray-100 via-50% to-gray-600 to-70% bg-clip-text text-transparent animate-gradient">
-        <div className="text-8xl font-medium mb-8 text-center">{'{ }'}</div>
+      {workbenchStore.previewsStore.isGenerationErrors.get() ? (
+        <GenerationError />
+      ) : (
+        <>
+          <div className="bg-gradient-to-r from-gray-600 from-30% via-gray-100 via-50% to-gray-600 to-70% bg-clip-text text-transparent animate-gradient">
+            <div className="text-8xl font-medium mb-8 text-center">{'{ }'}</div>
 
-        <div className="text-xl font-medium text-center">{message}</div>
-      </div>
+            <div className="text-xl font-medium text-center">{message}</div>
+          </div>
 
-      <style jsx>{`
-        .animate-gradient {
-          background-size: 200%;
-          -webkit-animation: animatedgradient 1s linear infinite;
-          -moz-animation: animatedgradient 1s linear infinite;
-          animation: animatedgradient 1s linear infinite;
-        }
+          <style jsx>{`
+            .animate-gradient {
+              background-size: 200%;
+              -webkit-animation: animatedgradient 1s linear infinite;
+              -moz-animation: animatedgradient 1s linear infinite;
+              animation: animatedgradient 1s linear infinite;
+            }
 
-        @keyframes animatedgradient {
-          100% {
-            background-position: -100% 0%;
-          }
-          0% {
-            background-position: 100% 0%;
-          }
-        }
-      `}</style>
+            @keyframes animatedgradient {
+              100% {
+                background-position: -100% 0%;
+              }
+              0% {
+                background-position: 100% 0%;
+              }
+            }
+          `}</style>
+        </>
+      )}
     </div>
   );
 };
