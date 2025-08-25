e1aa6faf8b5c34b48e79ed340a35c4331e99eab0
Cameron Steele <cam.steeleis@gmail.com>
2025-08-25 14:01:20 -0500

    Enable organizationClient plugin in auth client configuration

--- app/auth/auth-client.ts
+++ app/auth/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from 'better-auth/react';
 import { organizationClient } from 'better-auth/client/plugins';
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
-  plugins: [organizationClient()], // No plugins needed - we use built-in email/password and social auth
+  plugins: [organizationClient()],
 });
